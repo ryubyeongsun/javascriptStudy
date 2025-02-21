3c930c1a0240bad6df80d42987c22f58863166cf
unknown <ryubyeongsun>
2025-02-21 23:40:15 +0900

    getter_setter_static

---
+++ 2_class_and_oop/2_getter_and_setter.js
@@ -0,0 +1,59 @@
+/**
+ * Getter and Setter
+ */
+
+class IdolModel{
+    name;
+    year;
+
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+    
+    /**
+     * 1) 데이터를 가공해서 새로운 데이터를 반환할 때
+     * 2) private 한 값을 반환할 
+     *  get 선언하면 함수x 
+     */
+    get nameAndYear(){
+        return `${this.name}-${this.year}`;
+    }
+
+    set setName(name){
+        this.name = name;
+    }
+}
+
+const yuJin = new IdolModel('안유진', 2003);
+console.log(yuJin);
+console.log(yuJin.nameAndYear);
+
+yuJin.setName = '장원영';
+console.log(yuJin);
+
+class IdolModel2{
+    //#->private
+    #name;
+    year;
+
+    constructor(name, year){
+        this.#name = name;
+        this.year = year;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    set name(name){
+        this.#name = name;
+    }
+}
+
+const yuJin2 = new IdolModel2('안유진', 2003);
+console.log(yuJin2);
+console.log(yuJin2.name);
+
+yuJin2.name = '류병선';
+console.log(yuJin2.name);
