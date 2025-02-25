41783b13e34fad0d615a46c03468a59cdd5331c1
unknown <ryubyeongsun>
2025-02-25 23:35:17 +0900

    property_attribute

---
+++ 3_all_about_object/1_introducetion.js
@@ -0,0 +1,35 @@
+/**
+ * All about objects
+ * 
+ * 객체를 선언할 때 사용 할 수 있는 방법들
+ * 1) object를 생성해서 객체 생성 - 기본기 {}
+ * 2) class를 인스턴스화 해서 생성 - class와 OOP
+ * 3) function을 사용해서 객체 생성
+ */
+//1)번 방법
+const yuJin = {
+    name: '안유진',
+    year: 2003,
+};
+console.log(yuJin);
+
+//2)번 방법
+class IdolModel {
+    name;
+    year;
+
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+}
+console.log(new IdolModel('안유진', 2003));
+
+//3) 방법- 생성자 함수 - 함수엔에 this키워드 사용 이 경우에는 new로 함수 호출
+function IdolFunction(name, year){
+    this.name = name;
+    this.year = year;
+}
+
+const gaEul = new IdolFunction('가을', 2002);
+console.log(gaEul);
